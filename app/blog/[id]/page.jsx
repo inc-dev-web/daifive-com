88a28ed71de6554553b2924f2e7070d515f1bf7e
Limon <limon@MacBook-Pro-Limon.local>
2023-12-01 19:35:21 +0200

    add dunamic blog

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,20 +1,53 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BackButton } from '@/components/BackButton';
-import Image from 'next/image';
-import arrow from '@/public/image/icon/arrowDown.svg';
-import arrowUp from '@/public/image/icon/arrowUpBlue.svg';
 import imageArticles from '@/public/image/faqImage.png';
-import arrowBlue from '@/public/image/icon/ArrowRightBlue.svg';
-import arrowUpWhite from '@/public/image/icon/arrowUpWhite.svg';
 import ovalRed from '@/public/image/Oval-red.png';
 import ovalYellow from '@/public/image/Oval-yellow.png';
-import Link from 'next/link';
+import articleImg from '@/public/image/articles/articleTop.png';
 import whale from '@/public/image/whale.png';
 import author from '@/public/image/articles/author.png';
+import Image from 'next/image';
+import arrowBlue from '@/public/image/icon/ArrowRightBlue.svg';
+import Link from 'next/link';
 
-export default function Blog() {
-	const itemsArticles = [
+function ArticleText({ text, style }) {
+	const formatText = (paragraph) => {
+		const boldStart = '<b>';
+		const boldEnd = '</b>';
+		const boldIndexStart = paragraph.indexOf(boldStart);
+		const boldIndexEnd = paragraph.indexOf(boldEnd);
+
+		if (boldIndexStart !== -1 && boldIndexEnd !== -1) {
+			const boldText = paragraph.substring(boldIndexStart + boldStart.length, boldIndexEnd);
+			return (
+				<span>
+					<strong>{boldText}</strong>
+					{paragraph.substring(boldIndexEnd + boldEnd.length)}
+					<br />
+				</span>
+			);
+		} else {
+			return (
+				<span>
+					{paragraph}
+					<br />
+				</span>
+			);
+		}
+	};
+	return (
+		<p className={`${style}`}>
+			{text.split('\n').map((paragraph, index) => (
+				<span key={index}>{formatText(paragraph)}</span>
+			))}
+		</p>
+	);
+}
+
+export default function Articles({ params }) {
+	const articleId = params.id || 0;
+	const articles = [
 		{
 			imageLink: imageArticles,
 			id: 1,
@@ -221,172 +254,105 @@ export default function Blog() {
 			],
 		},
 	];
-
-	const [isOpen, setIsOpen] = useState(false);
-	const [selectedItem, setSelectedItem] = useState(itemsArticles[0].company);
-	const [screenWidth, setScreenWidth] = useState(null);
-	const [filteredArticles, setFilteredArticles] = useState([]);
-
-	const uniqueCompanies = Array.from(new Set(itemsArticles.map((item) => item.company)));
-
-	const toggleDropdown = () => {
-		if (screenWidth < 1024 || !isOpen) {
-			setIsOpen(!isOpen);
-		}
-	};
-
-	const handleItemClick = (item) => {
-		console.log('handleItemClick', item);
-		setSelectedItem(item);
-		console.log('setSelectedItem', selectedItem);
-		if (screenWidth < 1024) {
-			setIsOpen(false);
-		}
-		const filtered = itemsArticles.filter((article) => article.company === item);
-		setFilteredArticles(filtered);
-	};
-
-	useEffect(() => {
-		setScreenWidth(window.innerWidth);
-
-		const handleResize = () => {
-			setScreenWidth(typeof window !== 'undefined' ? window.innerWidth : 0);
-		};
-
-		setScreenWidth(typeof window !== 'undefined' ? window.innerWidth : 0);
-		window.addEventListener('resize', handleResize);
-
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
-
-	useEffect(() => {
-		setIsOpen(screenWidth >= 1024);
-	}, [screenWidth]);
-
-	useEffect(() => {
-		// Set filtered articles for the default selected item
-		const defaultFiltered = itemsArticles.filter((article) => article.company === selectedItem);
-		setFilteredArticles(defaultFiltered);
-	}, [selectedItem]);
+	let singleArticle = [articles[articleId]];
 
 	return (
-		<section className="px-4 pt-[39px] pb-[68px] lg:pt-[50px] lg:pb-[118px] lg:px-[50px] xl:px-[100px] relative">
+		<section className="px-4 pt-[39px] pb-[16px] relative lg:px-[50px] lg:pb-[50px] xl:px-[100px]">
+			<Image
+				src={ovalYellow}
+				alt="icon"
+				className="w-[18px] h-[18px] lg:w-[29px] lg:h-[29px] absolute top-0 lg:top-[32px] right-[115px] lg:right-[174px] -z-10"
+			/>
+			<Image
+				src={ovalRed}
+				alt="icon"
+				className="lg:block hidden w-[32px] h-[32px] absolute bottom-[105px] left-[93px] -z-10"
+			/>
 			<BackButton />
-			<div className="gap-3 lg:gap-4 my-4 lg:mt-8 lg:mb-12">
-				<h1 className="text-2xl lg:text-3xl xl:text-5xl font-bold text-[#2A333C]">Read Our Articles</h1>
-				<p className="text-xs xl:text-base text-[#7D7D7D]">Powerful Trading Tools and Features for Experienced Investors</p>
-			</div>
-			<div className="flex flex-col items-center lg:flex-row lg:items-start lg:gap-[32px]">
-				{/* Dropdown block */}
-				<div className={`w-full lg:max-w-[360px] bg-white rounded-[23px] lx:p-24`}>
-					<div
-						className={`lg:hidden px-3 text-sm flex items-center h-[48px] rounded-[84px] bg-white justify-between`}
-						onClick={toggleDropdown}
-					>
-						<div className="flex gap-3 justify-center items-center">
-							<div className="bg-blueRadianCustom w-[24px] h-[24px] flex justify-center items-center rounded-[32px]">
+			<div className="flex flex-col pt-4 lg:flex-row-reverse gap-8">
+				<div className="flex flex-col flex-1">
+					<Image
+						src={articleImg}
+						alt="img"
+						className="object-cover min-h-[160px] max-h-[228px] mb-6 rounded-[16px] lg:rounded-[24px]"
+					/>
+					{singleArticle.map((items, key) => (
+						<div
+							key={key}
+							className="flex-col flex rounded-[32px] px-4 py-6 lg:p-8 bg-white gap-8"
+						>
+							{items.details.map((item, index) => (
+								<div
+									key={index}
+									className="flex flex-col gap-[20px] px-4 lg:flex-row lg: items-start justify-between"
+								>
+									<div className="flex flex-row opacity-30 text-xs lg:text-base text-[#2A333C] gap-4">
+										<span>{item.dataCreate}</span>
+										<span>автор:</span>
+									</div>
+									<figure className="flex flex-row items-center gap-[6px] lg:gap-[16px] lg:mr-[270px] max-w-[253px]">
+										<Image
+											src={item.authorImage}
+											alt="img"
+											className="object-cover rounded-[32px] w-[32px] h-[32px] lg:w-[50px] lg:h-[50px]"
+										/>
+										<figcaption className="text-[#2A333C] flex flex-col opacity-30 gap-1">
+											<span className="text-xs lg:text-base font-medium">{item.author}</span>
+											<span className="text-[10px] lg:text-xs">{item.authorTitle}</span>
+										</figcaption>
+									</figure>
+								</div>
+							))}
+							<div>
+								<h4 className="text-[#2A333C] font-bold text-xl lg:text-custom32l mb-6">{items.title}</h4>
+								<ArticleText
+									text={items.article}
+									style={`text-[#2A333C] text-sm opacity-70 lg:text-base`}
+								/>
 								<Image
-									src={arrowUp}
-									alt="icon"
+									src={items.imageTitle}
+									alt="img"
+									className="mt-6 min-h-[230px] max-h-[504px] h-full"
 								/>
 							</div>
-							<span className="text-[#0B82FC] min-w-[268px] text-sm xl:text-base font-medium">
-								{selectedItem ? `${selectedItem}` : 'Category Name'}
-							</span>
 						</div>
-						<Image
-							src={arrow}
-							alt={'icon'}
-							width={24}
-							height={24}
-						/>
-					</div>
-					{isOpen && (
-						<ul className="flex gap-1 flex-col text-sm xl:text-base lg:p-6">
-							<h4 className="lg:block hidden text-[#2A333C] text-xl font-bold mb-4">Categories</h4>
-							{uniqueCompanies.map((item, index) => (
-								<li
-									className={`gap-[10px] flex hover:bg-[#F3F6FA] hover:[#0B82FC] hover:font-medium rounded-[70px] h-[48px] pl-[10px] items-center ${
-										selectedItem === item ? 'text-[#0B82FC] bg-[#F3F6FA]' : 'text-[#2a333c99]'
-									}`}
-									key={index}
-									onClick={() => handleItemClick(item)}
-								>
-									<div
-										className={`w-[24px] h-[24px] flex justify-center items-center rounded-[32px] ${
-											selectedItem === item ? 'bg-blueRadianCustom' : 'bg-[#00000019]'
-										}`}
-									>
-										<Image
-											src={selectedItem === item ? arrowUp : arrowUpWhite}
-											alt="icon"
-										/>
-									</div>
-									{item}
-								</li>
-							))}
-						</ul>
-					)}
+					))}
 				</div>
-
-				{/* Articles block */}
-				<div className="mt-4 flex flex-col gap-8 items-center lg:mt-0 lg:w-full lg:items-start">
-					{filteredArticles.map((item, index) => (
-						<Link href={`blog/${item.id}`}>
+				<div className="flex flex-row flex-wrap gap-10 mt-10 lg:mt-0 lg:flex-col lg:flex-nowrap lg:h-[1200px] overflow-y-scroll">
+					{articles.map((items, key) => (
+						<Link href={`${items.id}`}>
 							<div
-								className="flex flex-col lg:flex-row lg:items-center lg:w-full lg:max-h-[318px] lg:min-h-[318px]"
-								key={index}
+								key={key}
+								className="flex-col gap-6 max-w-[243px] lg:max-w-[297px]"
 							>
-								{/* Article image */}
-								<picture className="bg-white p-3 rounded-[24px] max-w-[343px] max-h-[282px] lg:min-h-[318px] lg:max-h-[318px] lg:min-w-[318px] lg:max-w-[318px]">
+								<picture className="flex p-3 bg-white rounded-[24px]">
 									<Image
-										src={item.imageLink}
-										alt="img"
-										className="object-center w-full h-full lg:min-h-[294px] lg:max-h-[294px] lg:min-w-[294px] lg:max-w-[294px]"
+										src={items.imageLink}
+										alt="icon"
+										className="max-h-[258px] lg:max-h-[224px] object-contain"
 									/>
 								</picture>
-								{/* Article details */}
-								<div className="flex flex-col px-5 lg:px-0 lg:pl-5 gap-3 max-w-[350px] lg:max-w-full">
-									<div className="mt-6 lg:mt-0 flex flex-col gap-3 min-h-[85px] lg:min-h-[160px] h-full">
-										<h4 className="text-[#2A333C] text-xl xl:text-3xl font-medium lg:font-bold">{item.title}</h4>
-										<p className="opacity-70 text-[#2A333C] text-xs xl:text-base">{item.description}</p>
-									</div>
-									{/* Read button */}
-									<div className="w-full flex items-center justify-center lg:justify-start">
-										<button className="bg-customBlueLight w-[270px] h-[48px] mt-3 rounded-[72px] flex items-center justify-center gap-2">
-											<span className="text-customBlue text-sm xl:text-lg">Read</span>
-											<Image
-												src={arrowBlue}
-												width={18}
-												height={18}
-												alt={'icon'}
-											/>
-										</button>
-									</div>
+
+								<div className="text-[#2A333C] mt-6 px-[20px]">
+									<h4 className="text-xl mb-3 font-medium">{items.title}</h4>
+									<p className="opacity-70 text-xs h-[60px] line-clamp-4 leading-[16px]">{items.description}</p>
+									<button
+										className={'lg:hidden h-[38px] bg-customBlueLight mt-6 w-[270px] flex items-center justify-center rounded-[65px] gap-3'}
+									>
+										<span className="text-customBlue text-sm">Читати</span>
+										<Image
+											src={arrowBlue}
+											width={18}
+											height={18}
+											alt={'icon'}
+										/>
+									</button>
 								</div>
 							</div>
 						</Link>
 					))}
 				</div>
 			</div>
-			<div className="lg:block hidden customRotate bg-radiant-blue absolute w-[900px] h-[500px] bottom-[0%] left-[-15%] -z-10"></div>
-			<Image
-				src={ovalRed}
-				alt="icon"
-				className="lg:block hidden w-[21px] h-[21px] absolute -z-10 left-[216px] bottom-[42%]"
-			/>
-			<Image
-				src={ovalRed}
-				alt="icon"
-				className="lg:block hidden w-[21px] h-[21px] absolute -z-10 right-[-120px] bottom-[34%]"
-			/>
-			<Image
-				src={ovalYellow}
-				alt="icon"
-				className="lg:block hidden w-[29px] h-[29px] absolute -z-10 left-[316px] bottom-[25%]"
-			/>
 		</section>
 	);
 }
