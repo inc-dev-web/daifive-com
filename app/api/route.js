e1dd8bbc8524386f6f70cd939ca7ae2be243dbaa
Artem Tarasenko <shabashab.04@gmail.com>
2023-12-04 16:23:16 +0200

    fix: edge runtime for /api

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function GET(request) {
 	const res = await fetch(`${process.env.API_URL}/${request}`);
 	const data = await res.json();
