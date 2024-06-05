93a1ad8e2594220618b4cf79748da2fa4be2618f
Oleg Tkach <KratosHome101@gmail.com>
2024-06-05 15:06:21 +0300

    feat: create help center component

--- app/public-organization/page.jsx
+++ app/public-organization/page.jsx
@@ -15,6 +15,7 @@ import target from '@/public/image/public-organization/target.png';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { enqueueSnackbar } from 'notistack';
+import HelpCenter from '@/components/HelpCenter';
 
 export default function publicOrganization() {
 	const router = useRouter();
@@ -202,42 +203,7 @@ export default function publicOrganization() {
 						<p className="text-gray-600 opacity-70 w-40 mx-auto">Дітей та батьків що відвідали наш центр</p>
 					</div>
 				</section>
-
-				<section className="relative w-full min-h-[586px] lg:min-h-[658px] mt-14 lg:mt-14 flex items-center justify-center">
-					<div className="absolute inset-0 w-full h-full -z-10 ">
-						<Image
-							src={background4}
-							alt="icon"
-							layout="fill"
-							className="object-cover rounded-xl"
-						/>
-					</div>
-					<div className="gap-4 relative flex flex-col items-center text-center bg-opacity-70 bg-blue-400 backdrop-blur-sm rounded-2xl mx-44 max-w-[812px]">
-						<div className="absolute bg-white rounded-full w-20 h-20 p-5 -top-10 left-1/2 transform -translate-x-1/2 flex items-center justify-center">
-							<Image
-								src={heart}
-								alt="icon"
-								width={40}
-								height={40}
-							/>
-						</div>
-						<h4 className="text-white font-bold text-2xl mt-14">Ви можете допомогти нашому центр</h4>
-						<div className="bg-blue-400 rounded-3xl text-white text-xs max-w-[90%] lg:max-w-[50%] p-4">
-							Подаруйте світло майбутньому наших дітей. Ваша пожертва - це ключ до їхнього щасливого та успішного життя. Приєднуйтеся до нас, щоб разом забезпечити їм необхідну підтримку та можливість розвитку в суспільстві. З деталями допомоги ви можете ознайомитись нижче у презентації.	
-						</div>
-						<button className="mx-5 flex justify-center items-center bg-customOrange rounded-[60px] text-base h-[56px] w-[226px] lg:w-[286px] text-[#FAFAFA] customBoxShadowOrange">
-							<Link href="#payments">Підтримати організацію</Link>
-						</button>
-						<button className="mb-10 gap-2 flex">
-							<Image
-								src={download}
-								alt={'icon'}
-								className={'inline-block'}
-							/>
-							<span className="inline-block text-white">Завантажити деталі</span>
-						</button>
-					</div>
-				</section>
+				<HelpCenter />
 			</div>
 			<section
 				className="flex flex-col items-center justify-center my-12 gap-5"
